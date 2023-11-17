5da8a99a0c6a87a2dc1028260f4ea2b33da121c3
the43rdsensei <ismailmuyideen.c05@gmail.com>
2023-11-17 10:32:00 +0100

    set markers to displays on corresponding cities and not at the center of the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,12 +3,14 @@ import { useNavigate, useSearchParams } from 'react-router-dom';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 import styles from './Map.module.css'
+import { useCities } from '../contexts/CitiesContext';
 
 function Map() {
   const [searchParams, setSearchParams] = useSearchParams()
+  const {cities} = useCities();
 
   const navigate = useNavigate()
-  const [mapPosition, setMapPosition] = useState([51.505, -0.09])
+  const [mapPosition, setMapPosition] = useState([40, 0])
 
   const lat = searchParams.get('lat')
   const lng = searchParams.get('lng')
@@ -20,11 +22,12 @@ function Map() {
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
     />
-    <Marker position={mapPosition}>
+    {cities.map((city) => <Marker position={[city.position.lat, city.position.lng]} key={city.id}>
       <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
+        <span>{city.emoji}</span>
+        <span>{city.cityName}</span>
       </Popup>
-    </Marker>
+    </Marker>)}
   </MapContainer>
     </div>
   )
