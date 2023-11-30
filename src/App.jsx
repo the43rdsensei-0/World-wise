9abe4a5ce84738e75145bfe479b814b21d6b407d
the43rdsensei <ismailmuyideen.c05@gmail.com>
2023-11-30 16:35:25 +0100

    added a protected route component, wrapping it around the app itself to prevent authorized access to the app, changed the url logo and page title

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import City from './components/City'
 import Form from './components/Form'
 import { CitiesProvider } from "./contexts/CitiesContext"
 import { AuthProvider } from "./contexts/FakeAuthContext"
+import ProtectedRoute from "./pages/ProtectedRoute"
 
 
 function App() {
@@ -25,7 +26,7 @@ function App() {
             <Route path="/" element={<Homepage />} />
             <Route path="product" element={<Product />} />
             <Route path="pricing" element={<Pricing />} />
-            <Route path="app" element={<AppLayout />}>
+            <Route path="app" element={<ProtectedRoute><AppLayout /></ProtectedRoute>}>
               <Route index element={<Navigate replace to={'cities'} />} />
               <Route path="cities" element={<CityList />} />
               <Route path="cities/:id" element={<City />}/>
