0965534f057864490232bdbe846493cf392a5ed2
the43rdsensei <ismailmuyideen.c05@gmail.com>
2023-12-03 14:06:54 +0100

    memoizing the getcity function to prevent it from being called infinitely in the City component

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useReducer } from "react";
+import { createContext, useCallback, useContext, useEffect, useReducer } from "react";
 
 const CitiesContext = createContext()
 const BASE_URL = 'http://localhost:8000'
@@ -82,7 +82,7 @@ function CitiesProvider({ children }) {
   fetchCities();
   }, []);
 
-  async function getCity(id) {
+  const getCity = useCallback(async function getCity(id) {
     if(Number(id) ===  currentCity.id) return;
 
     dispatch({type: 'loading'});
@@ -94,7 +94,7 @@ function CitiesProvider({ children }) {
     } catch {
       dispatch({type: 'rejected', payload: 'there was an error loading the city...'})
     } 
-  }
+  }, [currentCity.id])
 
   async function createCity(newCity ) {
     dispatch({type: 'loading'});
